2416ae21e4e464c9ff317c5c52eaf533eef9261f
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2022-11-06 13:47:50 -0600

    read_from_PCF8574_pins returns uint8_t

--- i2c_daisy_chain/main/i2c_LED_lights.h
+++ i2c_daisy_chain/main/i2c_LED_lights.h
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-//uint8_t read_from_PCF8574_pins(void); // read from pins
-void read_from_PCF8574_pins(); // read from pins
-
+uint8_t read_from_PCF8574_pins(void); // read from pins
 
 void write_to_PCF8574_pins(uint8_t data); // write to pins
