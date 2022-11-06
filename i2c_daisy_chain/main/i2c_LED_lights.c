3889dd99c6227482b0d759b2f26a652afb642042
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2022-11-06 13:44:39 -0600

    read_from_PCF8574_pins returns uint8_t

--- i2c_daisy_chain/main/i2c_LED_lights.c
+++ i2c_daisy_chain/main/i2c_LED_lights.c
@@ -9,30 +9,28 @@
 #include "esp_log.h"
 #include "driver/i2c.h"
 
-#define PCF8574_ADDR 0x21 // A0 jumper to ground
+#define PCF8574_SLAVE_ADDR 0x21 // A0 jumper to ground
 
 #define TIMEOUT_MS		1000
 #define DELAY_MS		1000
 
 
 static const char *TAG = "i2c_LED_lights";
-// this function should be modified to return data of type uint8_t
-//uint8_t read_from_PCF8574_pins()
-void read_from_PCF8574_pins()
+
+uint8_t read_from_PCF8574_pins()
 {
 
-//  Reference https://embeddedexplorer.com/esp32-i2c-tutorial/
-//  Reading from read_from_PCF8574_pins
+	//  Reference https://embeddedexplorer.com/esp32-i2c-tutorial/
+	//  Reading from read_from_PCF8574_pins
 	int len = 1;
-    uint8_t rx_data[len];
+    	uint8_t rx_data[len];
 
-//    while (1) {
-    	i2c_master_read_from_device(I2C_NUM_0, PCF8574_ADDR, rx_data, len, TIMEOUT_MS/portTICK_RATE_MS);
-    	ESP_LOG_BUFFER_HEX(TAG, rx_data, len);
-    	vTaskDelay(DELAY_MS/portTICK_RATE_MS);
-    	ESP_LOGI(TAG, "Read from PCF8574 pins");
-  //  }
-    //return rx_data;
+	i2c_master_read_from_device(I2C_NUM_0, PCF8574_SLAVE_ADDR, rx_data, len, TIMEOUT_MS/portTICK_RATE_MS);
+	ESP_LOG_BUFFER_HEX(TAG, rx_data, len);
+	vTaskDelay(DELAY_MS/portTICK_RATE_MS);
+	ESP_LOGI(TAG, "Read from PCF8574 pins");
+
+    	return rx_data[0]; // there is only one element in this array. Return it.
 }
 
 void write_to_PCF8574_pins(uint8_t data)
@@ -42,13 +40,15 @@ void write_to_PCF8574_pins(uint8_t data)
 
 	uint8_t data_t[len];
 	data_t[0] = data;
-	err = i2c_master_write_to_device(I2C_NUM_0, PCF8574_ADDR, data_t, len, 1000);
+	err = i2c_master_write_to_device(I2C_NUM_0, PCF8574_SLAVE_ADDR, data_t, len, 1000);
 
 	vTaskDelay(DELAY_MS/portTICK_RATE_MS);
-	if (err!=0) ESP_LOGI(TAG, "Error in sending data to PCF8574");
-
+	if (err!=0)
+		ESP_LOGI(TAG, "Error in sending data to PCF8574");
+	else
+		ESP_LOGI(TAG, "Write_to_PCF8574_pins successful");
 	// printf should be changed to ESP_LOGI
-	printf("write_to_PCF8574_pins successful \n");
+	// printf("Write_to_PCF8574_pins successful \n");
 
 }
 
