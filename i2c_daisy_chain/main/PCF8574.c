24d19974cf07ea9f5ee79d1b671c904522c100f1
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2022-11-06 16:15:21 -0600

    file name changed

--- i2c_daisy_chain/main/PCF8574.c
+++ i2c_daisy_chain/main/PCF8574.c
@@ -1,10 +1,11 @@
-/* i2c - LED lights example
-
-   Simple I2C example that shows LED lights blinking on PCF8574
-   by writing to it and reading from it over I2C.
+/*
+ * Wrapper i2c functions for reading from and writing to PCF8574
+ * Examples can be to light up LEDs on i2c.
+ * LED light on each pin of the PCF8574 can blink based on bits written to them.
+ * Pins can also be read likewise.
 */
 
-#include "i2c_LED_lights.h"
+#include <PCF8574.h>
 #include <stdio.h>
 #include "esp_log.h"
 #include "driver/i2c.h"
@@ -15,25 +16,25 @@
 #define DELAY_MS		1000
 
 
-static const char *TAG = "i2c_LED_lights";
+static const char *TAG = "PCF8574";
 
-uint8_t read_from_PCF8574_pins()
+uint8_t read_byte_from_pins()
 {
 
-	//  Reference https://embeddedexplorer.com/esp32-i2c-tutorial/
-	//  Reading from read_from_PCF8574_pins
+//  Reference https://embeddedexplorer.com/esp32-i2c-tutorial/
+//  Reading from read_from_PCF8574_pins
 	int len = 1;
-    	uint8_t rx_data[len];
+	uint8_t rx_data[len];
 
 	i2c_master_read_from_device(I2C_NUM_0, PCF8574_SLAVE_ADDR, rx_data, len, TIMEOUT_MS/portTICK_RATE_MS);
 	ESP_LOG_BUFFER_HEX(TAG, rx_data, len);
 	vTaskDelay(DELAY_MS/portTICK_RATE_MS);
-	ESP_LOGI(TAG, "Read from PCF8574 pins");
+	ESP_LOGI(TAG, "read_byte_from_pins");
 
-    	return rx_data[0]; // there is only one element in this array. Return it.
+	return rx_data[0]; // there is only one element in this array. Return it.
 }
 
-void write_to_PCF8574_pins(uint8_t data)
+void write_byte_to_pins(uint8_t data)
 {
 	esp_err_t err;
 	int len = 1;
@@ -46,9 +47,8 @@ void write_to_PCF8574_pins(uint8_t data)
 	if (err!=0)
 		ESP_LOGI(TAG, "Error in sending data to PCF8574");
 	else
-		ESP_LOGI(TAG, "Write_to_PCF8574_pins successful");
+		ESP_LOGI(TAG, "Write_bytes_to_pins successful");
 	// printf should be changed to ESP_LOGI
 	// printf("Write_to_PCF8574_pins successful \n");
 
 }
-
