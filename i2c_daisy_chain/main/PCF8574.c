ffb4bd51f5bce3afab6f378204a184241c485126
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2022-11-07 14:23:14 -0600

    remove "int len" local variable

--- i2c_daisy_chain/main/PCF8574.c
+++ i2c_daisy_chain/main/PCF8574.c
@@ -23,11 +23,10 @@ uint8_t read_byte_from_pins()
 
 //  Reference https://embeddedexplorer.com/esp32-i2c-tutorial/
 //  Reading from read_from_PCF8574_pins
-	int len = 1;
-	uint8_t rx_data[len];
+	uint8_t rx_data[1];
 
-	i2c_master_read_from_device(I2C_NUM_0, PCF8574_SLAVE_ADDR, rx_data, len, TIMEOUT_MS/portTICK_RATE_MS);
-	ESP_LOG_BUFFER_HEX(TAG, rx_data, len);
+	i2c_master_read_from_device(I2C_NUM_0, PCF8574_SLAVE_ADDR, rx_data, 1, TIMEOUT_MS/portTICK_RATE_MS);
+	ESP_LOG_BUFFER_HEX(TAG, rx_data, 1);
 	vTaskDelay(DELAY_MS/portTICK_RATE_MS);
 	ESP_LOGI(TAG, "read_byte_from_pins");
 
@@ -48,7 +47,5 @@ void write_byte_to_pins(uint8_t data)
 		ESP_LOGI(TAG, "Error in sending data to PCF8574");
 	else
 		ESP_LOGI(TAG, "Write_bytes_to_pins successful");
-	// printf should be changed to ESP_LOGI
-	// printf("Write_to_PCF8574_pins successful \n");
 
 }
