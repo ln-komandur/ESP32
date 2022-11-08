4d1fbe5edc7ec1ae8fdd2991c4027b38420102f7
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2022-11-08 16:39:37 -0600

    working functions

--- i2c_daisy_chain/main/i2c-matrix-keypad.c
+++ i2c_daisy_chain/main/i2c-matrix-keypad.c
@@ -2,32 +2,48 @@
 
 // Yet to be implemented
 
-/*
-#include <i2c_LCD.h>
 #include <stdio.h>
+#include "i2c-matrix-keypad.h"
+
 #include "esp_log.h"
+
 #include "driver/i2c.h"
 
-#define SLAVE_ADDRESS_KPD 0x21 // A0 jumper high for keypad
+#define PCF8574_SLAVE_KPD_ADDRESS 0x20 // No jumper set for keypad on PCF8574 expander
 
-#define I2C_SLAVE_ADDR	0x11
 #define TIMEOUT_MS		1000
 #define DELAY_MS		1000
 
 
 esp_err_t err;
 
-#define I2C_NUM I2C_NUM_0
-
 static const char *TAG = "Matrix Keypad";
 
-void read_keypad_app_main() {
-	uint8_t rx_data[5];
+uint8_t read_keypad_pins() {
+	//  Reference https://embeddedexplorer.com/esp32-i2c-tutorial/
+	//  Reading from read_from_PCF8574_pins
+	uint8_t rx_data[1];
 
-	while (1) {
-		i2c_master_read_from_device(I2C_NUM, I2C_SLAVE_ADDR, rx_data, 5, TIMEOUT_MS/portTICK_RATE_MS);
-		ESP_LOG_BUFFER_HEX(TAG, rx_data, 5);
-		vTaskDelay(DELAY_MS/portTICK_RATE_MS);
-	}
+	i2c_master_read_from_device(I2C_NUM_0, PCF8574_SLAVE_KPD_ADDRESS, rx_data, 5, TIMEOUT_MS/portTICK_RATE_MS);
+	vTaskDelay(DELAY_MS/portTICK_RATE_MS);
+	ESP_LOGI(TAG, "read_keypad_pins");
+	ESP_LOG_BUFFER_HEX(TAG, rx_data, 1);
+
+	return rx_data[0]; // there is only one element in this array. Return it.
 }
-*/
+
+void write_to_keypad(uint8_t data)
+{
+	int len = 1;
+
+	uint8_t data_t[len];
+	data_t[0] = data;
+	err = i2c_master_write_to_device(I2C_NUM_0, PCF8574_SLAVE_KPD_ADDRESS, data_t, len, 1000);
+
+	vTaskDelay(DELAY_MS/portTICK_RATE_MS);
+	if (err!=0)
+		ESP_LOGI(TAG, "Error in sending data to PCF8574 keypad");
+	else
+		ESP_LOGI(TAG, "write_to_keypad successful");
+
+}
