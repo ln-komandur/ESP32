45cc032a8167354797052250eaf2eaac5ccf713c
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2022-11-15 12:04:53 -0600

    moved interrupt handling to i2c-matrix-keypad

--- i2c_daisy_chain/main/i2c_daisy_chain_main.c
+++ i2c_daisy_chain/main/i2c_daisy_chain_main.c
@@ -9,47 +9,16 @@
 #include <stdio.h>
 #include "esp_log.h"
 #include "driver/i2c.h"
-
-// the below are from https://esp32tutorials.com/esp32-gpio-interrupts-esp-idf/
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/queue.h"
-#include "driver/gpio.h"
-// the above are from https://esp32tutorials.com/esp32-gpio-interrupts-esp-idf/
+#include "esp_timer.h"
 
 #define DELAY_MS		1000
 
-// the below are from https://esp32tutorials.com/esp32-gpio-interrupts-esp-idf/
-#define INPUT_PIN 15
-/* The interrupt pin 'INT' from PCF8574 is connected to GPIO15 of ESP32.
- * ‘INPUT_PIN’ is used to read the digital input from pin number 15.
- */
-
-// the above are from https://esp32tutorials.com/esp32-gpio-interrupts-esp-idf/
-
 static const char *TAG = "i2c-daisy-chain-example";
 
-static bool isKeyBeingRead = false;
+uint8_t aByte = 0x55; //  binary of 0x55 is 01010101
 
-// the below are from https://esp32tutorials.com/esp32-gpio-interrupts-esp-idf/
-int state = 0;
-xQueueHandle interruptQueue;
-
-/* Inside the interrupt service routine we will call the xQueueSendFromISR() function.
- * It will send an item from a queue. This function takes in 3 parameters.
- * The 1st is ‘xQueue’ which is the queue handle on which the item is to be posted.
- * In our case it is ‘interuptQueue.’
- * The second parameter is the ‘pvItemToQueue’ which is the pointer to the item that is placed on the queue.
- * In our case it is ‘&pinNumber.’ The third parameter is the ‘pxHigherPriorityTaskWoken’ which is NULL in our case.
- */
-static void IRAM_ATTR gpio_interrupt_handler(void *args)
-{
-	if (!isKeyBeingRead) // if a key is not being read, then add an item to the queue
-	{
-		int pinNumber = (int)args;
-		xQueueSendFromISR(interruptQueue, &pinNumber, NULL);
-	}
-}
+int64_t lastBlink;
+long blinkDuration = 500000; // 500000 is in microseconds for 0.5 seconds
 
 void write_string_on_LCD(int lineNo, int colNo, char *str)
 {
@@ -71,8 +40,9 @@ void write_hex_on_LCD(int lineNo, int colNo, uint8_t hex)
 
 void blink_LEDs_by_bit_shift()
 {
-	uint8_t aByte = 0x55; //  binary of 0x55 is 01010101
-	uint8_t aFoundByte[1]; //  used for storing what is read from PCF8574
+
+    int64_t callTime = esp_timer_get_time();
+
 	/*
 	 * 0x55 = 01010101, which,
 	 * when shifted left once, it gives 0xaa = 10101010, which
@@ -84,18 +54,14 @@ void blink_LEDs_by_bit_shift()
 	 * when shifted left once, it gives 0x80 = 10000000, which
 	 * when shifted left once, it gives 0x00 = 00000000, when the byte is reset to 0x55
 	 */
-	while (true)
+	if (callTime - lastBlink > blinkDuration)
 	{
 		write_byte_to_pins(aByte);
 		aByte = aByte << 1;
 		if (aByte == 0x00) {
 			aByte = 0x55;
 		}
-		aFoundByte[0] = read_byte_from_pins();
-		ESP_LOGI(TAG, "Read byte from PCF8574 pins");
-		ESP_LOG_BUFFER_HEX(TAG, aFoundByte , 1);
-
-		write_hex_on_LCD(1, 0, aFoundByte[0]); // display it on line 2 of the LCD though as decimal
+		lastBlink = callTime;
 	}
 }
 
@@ -104,74 +70,6 @@ void show_byte_with_LEDs(uint8_t aByte)
 	write_byte_to_pins(aByte);
 	ESP_LOGI(TAG, "show_byte_with_LEDs");
 }
-
-void showKeyPressed(void) // this method may be called based on interrupt. Yet to explore
-{
-	char pressedKey; //  used for storing what is read from PCF8574 keypad
-	pressedKey = find_key();
-	char buffer[16];
-	sprintf(buffer, "Pressed %c", pressedKey); // display hexadecimal
-	write_string_on_LCD(1, 0, buffer); // display it on line 2 of the LCD though as string
-
-	//vTaskDelay(DELAY_MS/portTICK_RATE_MS); // Show the key name on the LCD for a short time
-}
-
-
-
-/* Inside this function, we first create two integer variables ‘pinNumber’ and ‘count’ and
- * set their value to 0. Then inside the infinite while function, we check if an item is
- * received from the queue by using the xQueueReceive() function. This function takes in
- * three parameters. The first parameter is the ‘xQueue’ which is the queue handle from which
- * the item is to be received. It is ‘interuptQueue’ in our case. The second parameter is the
- * ‘pvBuffer’ which is the pointer to the buffer into which the received item will be copied.
- * In our case it is ‘&pinNumber.’ The third parameter is the ‘xTicksToWait’ which is the
- * maximum amount of time the task should block waiting for an item to receive should the
- * queue be empty at the time of the call. In our case it is set as ‘portMAX_DELAY.’
- * Thus, when the interrupt occurs then gpio_set_level() function is called to set the level of
- * the LED_PIN according to the level of the INPUT_PIN.
- * At this time the ESP-IDF terminal prints the message that the GPIO was pressed,
- * its state, the pin Number and count.
- * The above are from https://esp32tutorials.com/esp32-gpio-interrupts-esp-idf/
- * It also calls the showKeyPressed() function to display the key
- */
-
-
-void Key_Ctrl_Task(void *params) // this function will be called when interrupt happens.
-{
-	int pinNumber = 0;
-	int count = 0;
-
-	while (true)
-	{
-		if (xQueueReceive(interruptQueue, &pinNumber, portMAX_DELAY))
-		{
-			int pinState = gpio_get_level(INPUT_PIN);
-			printf("GPIO%d state is %d. isKeyBeingRead %d\n", pinNumber, pinState, isKeyBeingRead);
-
-
-			if (pinState == 0) // only when pin goes down (Falling edge)
-			{
-				isKeyBeingRead = true; // block more items from being added to the queue until this key is found
-				printf("isKeyBeingRead is blocked %d\n", isKeyBeingRead);
-
-				ESP_LOGI(TAG, "Key Pressed on PCF8574 keypad ");
-				showKeyPressed(); // call show key only if not already working on it
-				isKeyBeingRead = false; // reset flag so that the next key press can be added to the queue
-				printf("GPIO%d was pressed %d times. Pin state is %d. isKeyBeingRead is released %d\n", pinNumber, ++count, pinState, isKeyBeingRead);
-
-			}
-		}
-		else
-		{
-			ESP_LOGI(TAG, "Key NOT Pressed on PCF8574 keypad ");
-			write_string_on_LCD(1, 0, "None pressed "); // display it on line 2 of the LCD though as string
-		}
-	}
-	//vTaskDelete(NULL); // added per https://stackoverflow.com/questions/63634917/freertos-task-should-not-return-esp32
-
-}
-
-
 
 
 void app_main(void)
@@ -185,54 +83,26 @@ void app_main(void)
 
 	write_string_on_LCD(0,0, "Matrix keypad");
 
-	set_keypad_pins(0xf0); // write 11110000
+	setup_keypad();
 
-	// the below are from https://esp32tutorials.com/esp32-gpio-interrupts-esp-idf/
-	/*
-	 * This way when the push button will be pressed, the state of the INPUT_PIN will
-	 * go HIGH and when it will be released then the state of the INPUT_PIN will go LOW.
-	 *
-	 */
-	// We configure the INPUT_PIN as a GPIO and then set its direction as an input.
-	gpio_pad_select_gpio(INPUT_PIN);
-	gpio_set_direction(INPUT_PIN, GPIO_MODE_INPUT);
+	lastBlink = esp_timer_get_time();
 
+	while (true)
+	{
+		char keyPressed = 0;
 
+		xQueueReceive(keyQueue, &keyPressed, ( portTickType ) 0); // put the key found in the queue
+		if (keyPressed != 0)
+		{
+			char buffer[16];
+			sprintf(buffer, "Last pressed %c", keyPressed); // display hexadecimal
+			write_string_on_LCD(1, 0, buffer); // display it on line 2 of the LCD though as string
+			// vTaskDelay(DELAY_MS/portTICK_RATE_MS); // Show the key name on the LCD for a short time
 
-	/* We enable the pullup resistor and disable the pulldown resistor on the input pin.
-	 * https://www.best-microcontroller-projects.com/pcf8574.html says enable pull-up
-	 * https://www.mischianti.org/2019/01/02/pcf8574-i2c-digital-i-o-expander-fast-easy-usage/
-	 * The interrupt open-drain output pin is active LOW.  It is normally pulled HIGH
-	 * using a pull-up resistor and is driven low by the PCF8574 when any of the inputs
-	 * change state. This signals the MCU to poll the part to see what is going on.
-	 * If connecting this pin, enable the internal pull-up resistor on the MCU or add
-	 * an external pull-up of 10K or so.
-	 */
+		}
 
-	gpio_pulldown_dis(INPUT_PIN);
-	gpio_pullup_en(INPUT_PIN);
+		blink_LEDs_by_bit_shift();
 
-	// Then, we will set the interrupt trigger type on the INPUT_PIN using gpio_set_intr_type() function.
-	// This function takes in two parameters.
-	// The first parameter is the GPIO pin number which is ‘INPUT_PIN’ in our case.
-	// The second parameter is the trigger type which is set as ‘GPIO_INTR_NEGEDGE’ (falling)
-	// which means the interrupt will be triggered when the the state of the pin changes from HIGH to LOW.
-	// Refer falling edge per https://www.best-microcontroller-projects.com/pcf8574.html
+	}
 
-	gpio_set_intr_type(INPUT_PIN, GPIO_INTR_NEGEDGE);
-
-	/* Create a queue that can hold 10 elements, each of size int.
-	 * Then create a task using xTaskCreate with necessary arguments. Refer https://esp32tutorials.com/esp32-gpio-interrupts-esp-idf/
-	 */
-	interruptQueue = xQueueCreate(10, sizeof(int));
-	xTaskCreate(Key_Ctrl_Task, "Key_Ctrl_Task", 2048, NULL, 1, NULL);
-	/* Install the interrupt service routine with the flags for interrupt allocation
-	 * Add the ISR handler for the INPUT_PIN using gpio_isr_handler_add()
-	 */
-	gpio_install_isr_service(0);
-	gpio_isr_handler_add(INPUT_PIN, gpio_interrupt_handler, (void *)INPUT_PIN);
-
-	// the above are from https://esp32tutorials.com/esp32-gpio-interrupts-esp-idf/
-
-	//blink_LEDs_by_bit_shift();
 }
