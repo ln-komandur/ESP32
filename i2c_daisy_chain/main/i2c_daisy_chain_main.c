4578f5449d773e55496854e61652d82ddae485f3
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2022-11-16 14:25:41 -0600

    moved LCD and LED functions to other task modules

--- i2c_daisy_chain/main/i2c_daisy_chain_main.c
+++ i2c_daisy_chain/main/i2c_daisy_chain_main.c
@@ -15,61 +15,6 @@
 
 static const char *TAG = "i2c-daisy-chain-example";
 
-uint8_t aByte = 0x55; //  binary of 0x55 is 01010101
-
-int64_t lastBlink;
-long blinkDuration = 500000; // 500000 is in microseconds for 0.5 seconds
-
-void write_string_on_LCD(int lineNo, int colNo, char *str)
-{
-	lcd_put_cur(lineNo, colNo);
-	lcd_send_string("				"); // Erases the existing content fully
-
-	lcd_put_cur(lineNo, colNo);
-	lcd_send_string(str);
-}
-
-void write_hex_on_LCD(int lineNo, int colNo, uint8_t hex)
-{
-	char buffer[16];
-	sprintf(buffer, "0x%02X", hex); // display hexadecimal
-
-	lcd_put_cur(lineNo, colNo);
-	lcd_send_string(buffer);
-}
-
-void blink_LEDs_by_bit_shift()
-{
-
-    int64_t callTime = esp_timer_get_time();
-
-	/*
-	 * 0x55 = 01010101, which,
-	 * when shifted left once, it gives 0xaa = 10101010, which
-	 * when shifted left once, it gives 0x54 = 01010100, which
-	 * when shifted left once, it gives 0xa8 = 10101000, which
-	 * when shifted left once, it gives 0x50 = 01010000, which
-	 * when shifted left once, it gives 0xa0 = 10100000, which
-	 * when shifted left once, it gives 0x40 = 01000000, which
-	 * when shifted left once, it gives 0x80 = 10000000, which
-	 * when shifted left once, it gives 0x00 = 00000000, when the byte is reset to 0x55
-	 */
-	if (callTime - lastBlink > blinkDuration)
-	{
-		write_byte_to_pins(aByte);
-		aByte = aByte << 1;
-		if (aByte == 0x00) {
-			aByte = 0x55;
-		}
-		lastBlink = callTime;
-	}
-}
-
-void show_byte_with_LEDs(uint8_t aByte)
-{
-	write_byte_to_pins(aByte);
-	ESP_LOGI(TAG, "show_byte_with_LEDs");
-}
 
 
 void app_main(void)
@@ -85,24 +30,7 @@ void app_main(void)
 
 	setup_keypad();
 
-	lastBlink = esp_timer_get_time();
+	init_LCD(keyQueue);
 
-	while (true)
-	{
-		char keyPressed = 0;
-
-		xQueueReceive(keyQueue, &keyPressed, ( portTickType ) 0); // put the key found in the queue
-		if (keyPressed != 0)
-		{
-			char buffer[16];
-			sprintf(buffer, "Last pressed %c", keyPressed); // display hexadecimal
-			write_string_on_LCD(1, 0, buffer); // display it on line 2 of the LCD though as string
-			// vTaskDelay(DELAY_MS/portTICK_RATE_MS); // Show the key name on the LCD for a short time
-
-		}
-
-		blink_LEDs_by_bit_shift();
-
-	}
-
+	init_LED_Byte(0x55); //  binary of 0x55 is 01010101
 }
