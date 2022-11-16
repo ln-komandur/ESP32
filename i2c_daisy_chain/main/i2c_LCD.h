ea8aee141ed692aab6272bbbee53646116b3fbfe
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2022-11-16 14:17:42 -0600

    LCD functions to support task handling

--- i2c_daisy_chain/main/i2c_LCD.h
+++ i2c_daisy_chain/main/i2c_LCD.h
@@ -11,3 +11,11 @@ void lcd_send_string (char *str);  // send string to the lcd
 void lcd_put_cur(int row, int col);  // put cursor at the entered position row (0 or 1), col (0-15);
 
 void lcd_clear (void);
+
+void write_string_on_LCD(int lineNo, int colNo, char *str);
+
+void write_hex_on_LCD(int lineNo, int colNo, uint8_t hex);
+
+void init_LCD(xQueueHandle keyQueue);
+
+
