99aab29bcdf57065d26414b6cd3a5f636b078844
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2022-11-16 15:02:43 -0600

    included blinkDuration to initialize

--- i2c_daisy_chain/main/PCF8574.h
+++ i2c_daisy_chain/main/PCF8574.h
@@ -4,4 +4,4 @@ uint8_t read_byte_from_pins(void); // read byte from pins
 
 void write_byte_to_pins(uint8_t data); // write byte to pins
 
-void init_LED_Byte(uint8_t startByte);
+void init_LED(uint8_t startByte, long duration);
