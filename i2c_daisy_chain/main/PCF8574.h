ce85aa36d85884f236e14f6e1b6dc4ed2f77bd17
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2024-12-29 13:39:09 -0600

    Ported to FreeRTOS v8.0.0, esp-idf-v5.3.1

--- i2c_daisy_chain/main/PCF8574.h
+++ i2c_daisy_chain/main/PCF8574.h
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include "esp_timer.h"
 
 uint8_t read_byte_from_pins(void); // read byte from pins
 
 void write_byte_to_pins(uint8_t data); // write byte to pins
 
 void init_LED(uint8_t startByte, long duration);
+
+extern long blinkDuration; // in microseconds
+
+extern esp_err_t err;
