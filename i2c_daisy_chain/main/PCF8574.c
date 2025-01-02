7b5f8cc989088afe12280e10c8871529be258eda
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2025-01-02 15:31:20 -0600

    LEDs separated from PCF8574, i2c port number

--- i2c_daisy_chain/main/PCF8574.c
+++ i2c_daisy_chain/main/PCF8574.c
@@ -1,8 +1,6 @@
 /*
- * Wrapper i2c functions for reading from and writing to PCF8574
- * Examples can be to light up LEDs on i2c.
- * LED light on each pin of the PCF8574 can blink based on bits written to them.
- * Pins can also be read likewise.
+ * Functions for reading from and writing to PCF8574 at an i2c port number
+ * 
 */
 
 #include <PCF8574.h>
@@ -12,21 +10,17 @@
 
 #define TIMEOUT_MS		1000
 
-
 static const char *TAG = "PCF8574";
 
-uint8_t aByte;
 
-int64_t lastBlink;
-
-uint8_t read_byte_from_pins()
+uint8_t read_byte_from_pins(i2c_port_t i2c_port_num)
 {
 
 //  Reference https://embeddedexplorer.com/esp32-i2c-tutorial/
 //  Reading from read_from_PCF8574_pins
 	uint8_t rx_data[1];
 
-	i2c_master_read_from_device(I2C_NUM_0, PCF8574_SLAVE_ADDR, rx_data, 1, TIMEOUT_MS/portTICK_PERIOD_MS);
+	i2c_master_read_from_device(i2c_port_num, PCF8574_SLAVE_ADDR, rx_data, 1, TIMEOUT_MS/portTICK_PERIOD_MS);
 	ESP_LOG_BUFFER_HEX(TAG, rx_data, 1);
 
 	ESP_LOGI(TAG, "read_byte_from_pins");
@@ -34,62 +28,16 @@ uint8_t read_byte_from_pins()
 	return rx_data[0]; // there is only one element in this array. Return it.
 }
 
-void write_byte_to_pins(uint8_t data)
+void write_byte_to_pins(i2c_port_t i2c_port_num, uint8_t data)
 {
 	int len = 1;
 
 	uint8_t data_t[len];
 	data_t[0] = data;
-	err = i2c_master_write_to_device(I2C_NUM_0, PCF8574_SLAVE_ADDR, data_t, len, 1000);
+	err = i2c_master_write_to_device(i2c_port_num, PCF8574_SLAVE_ADDR, data_t, len, 1000);
 
-	if (err!=0) ESP_LOGI(TAG, "Error in sending data to PCF8574");
-	/* else
-		ESP_LOGI(TAG, "Write_bytes_to_pins successful");
-	*/
-}
-
-void show_byte_with_LEDs(uint8_t aByte)
-{
-	write_byte_to_pins(aByte);
-	ESP_LOGI(TAG, "show_byte_with_LEDs");
-}
-void blink_LEDs_Task(void *params)
-{
-	lastBlink = esp_timer_get_time();
-
-	while (true)
-	{
-	    int64_t callTime = esp_timer_get_time();
-
-		/*
-		 * 0x55 = 01010101, which,
-		 * when shifted left once, it gives 0xaa = 10101010, which
-		 * when shifted left once, it gives 0x54 = 01010100, which
-		 * when shifted left once, it gives 0xa8 = 10101000, which
-		 * when shifted left once, it gives 0x50 = 01010000, which
-		 * when shifted left once, it gives 0xa0 = 10100000, which
-		 * when shifted left once, it gives 0x40 = 01000000, which
-		 * when shifted left once, it gives 0x80 = 10000000, which
-		 * when shifted left once, it gives 0x00 = 00000000, when the byte is reset to 0x55
-		 */
-		if (callTime - lastBlink > blinkDuration)
-		{
-			write_byte_to_pins(aByte);
-			aByte = aByte << 1;
-			if (aByte == 0x00) {
-				aByte = 0x55;
-			}
-			lastBlink = callTime;
-		}
-	}
-	vTaskDelete(NULL); // added per https://stackoverflow.com/questions/63634917/freertos-task-should-not-return-esp32 at the end of the function to gracefully end the task:
-}
-
-
-void init_LED(uint8_t startByte, long duration)
-{
-	xTaskCreate(blink_LEDs_Task, "blink_LEDs_Task", 2048, NULL, 1, NULL);
-	blinkDuration = duration;
-	aByte = startByte;
-
+	if (err!=0) 
+	    ESP_LOGI(TAG, "Error in sending data to PCF8574");
+	else
+	    ESP_LOGI(TAG, "Write_bytes_to_pins successful");
 }
