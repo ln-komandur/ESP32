6195c3438eff617855936f138ea30013f6e98e3b
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2025-01-02 15:29:01 -0600

    LEDs separated from PCF8574, i2c_port_num

--- i2c_daisy_chain/main/PCF8574.h
+++ i2c_daisy_chain/main/PCF8574.h
@@ -1,14 +1,11 @@
+#include "hal/i2c_types.h"
 #include <esp_err.h>
 #include <esp_log.h>
 #include <driver/i2c.h>
 #include <esp_timer.h>
 
-uint8_t read_byte_from_pins(void); // read byte from pins
+uint8_t read_byte_from_pins(i2c_port_t i2c_port_num); // read byte from pins at i2c port number
 
-void write_byte_to_pins(uint8_t data); // write byte to pins
-
-void init_LED(uint8_t startByte, long duration);
-
-extern long blinkDuration; // in microseconds
+void write_byte_to_pins(i2c_port_t i2c_port_num, uint8_t data); // write byte to pins at i2c port number
 
 extern esp_err_t err;
