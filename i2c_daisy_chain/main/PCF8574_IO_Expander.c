0df5b7aab63ee9e3a72c93be8b794ad12f2b5d95
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2025-01-07 16:05:11 -0600

    ported to new driver/i2c_master.h ; renamed

--- i2c_daisy_chain/main/PCF8574_IO_Expander.c
+++ i2c_daisy_chain/main/PCF8574_IO_Expander.c
@@ -13,7 +13,6 @@
 
 static const char *TAG = "PCF8574_IO_Expander";
 
-
 i2c_master_dev_handle_t get_i2c_device_handle(i2c_master_bus_handle_t bus_handle, uint8_t dev_address)
 {
 	i2c_master_dev_handle_t device_handle;
@@ -28,26 +27,20 @@ i2c_master_dev_handle_t get_i2c_device_handle(i2c_master_bus_handle_t bus_handle
 	// Add the device to the master bus
 	i2c_master_bus_add_device(bus_handle, &pcf8574_cfg, &device_handle);
 	ESP_LOGI(TAG, "get_i2c_device_handle: Got a Device Handle: About to return that DEVICE handle");
-
-	return device_handle;
+	return device_handle; // Return the device handle
 }
 
 uint8_t read_byte_from_pins(i2c_master_dev_handle_t device_handle)
 {
-	// This happens in the IO expander when reading data using it
 	uint8_t rx_data[1];
-	
 	// Receive / Read from the device
 	i2c_master_receive(device_handle, rx_data, 1, -1); // -1 means wait forever.
-	ESP_LOG_BUFFER_HEX(TAG, rx_data, 1);
-
 	ESP_LOGI(TAG, "read byte from PCF8574 pins at its device handle. About to return that byte");
 	return rx_data[0]; // there is only one element in this array. Return it.
-
 }
+
 void write_byte_to_pins(i2c_master_dev_handle_t device_handle, uint8_t data)
 {
-	// This happens in the IO expander when writing data using it
 	// Transmit / Write to the device
 	int len = 1;
 	uint8_t data_t[len];
@@ -55,5 +48,4 @@ void write_byte_to_pins(i2c_master_dev_handle_t device_handle, uint8_t data)
 	i2c_master_transmit(device_handle, data_t, len, -1); // -1 means wait forever. This call returns ESP_ERR_INVALID_STATE.
 	// esp_rom_delay_us(100); // Per BQ27441 datasheet. - DONT KNOW HOW TO ADAPT IT TO PCF8574 from https://github.com/espressif/esp-idf/issues/14401
 	//ESP_LOGI(TAG, "wrote byte to PCF8574 pins at its device handle. About to return");
-
 }
