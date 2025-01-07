b74aa7a9d9470cbec2b802437f5eabf3b9f74361
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2025-01-07 16:23:10 -0600

    ported to new driver/i2c_master.h

--- i2c_daisy_chain/main/i2c-matrix-keypad.h
+++ i2c_daisy_chain/main/i2c-matrix-keypad.h
@@ -1,12 +1,29 @@
-// Good reference - https://embeddedexplorer.com/esp32-i2c-tutorial/
+/*
+ * i2c_mast_handler.h
+ *
+ *  Created on: Jan 1, 2025
+ *      Author: Ln-Komandur
+ *
+ * Manages (methods to initialize, check) i2c ports for ESP32
+ * Reference - https://embeddedexplorer.com/esp32-i2c-tutorial/ - The code here is originally from https://gist.github.com/herzig/8d4c13d8b81a77ac86481c6c1306bb12
+ */
+
 
 #include <esp_err.h>
-#include <freertos/FreeRTOS.h>
 #include <esp_log.h>
-#include <driver/i2c.h>
+#include <driver/i2c_master.h>
+#include <freertos/FreeRTOS.h>
 
-extern esp_err_t err;
+#define MAX_NO_OF_I2C_DEVICES 128 // This is the maximum number of I2C devices that can be connected
 
-extern QueueHandle_t keyQueue; // used for storing what is read from PCF8574 keypad
 
-void init_keypad(i2c_port_t i2c_port_num); // this will assign the i2c port, and initialize data pins as well as interrupt pin
+/*
+ * Initializes an i2c bus at the given i2c port number and returns the bus handle
+ */
+i2c_master_bus_handle_t get_i2c_bus_handle(int i2c_num);
+
+
+/*
+ * Returns a pointer to an array of i2c_device_addresses for the given bus handle
+ */
+uint8_t* get_connected_i2c_device_addresses(i2c_master_bus_handle_t bus_handle);
