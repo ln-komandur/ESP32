40f10a8450a995442ac7cb05837d3cc257b36b99
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2025-01-07 16:20:44 -0600

    ported to new driver/i2c_master.h

--- i2c_daisy_chain/main/a_byte_of_LEDs.c
+++ i2c_daisy_chain/main/a_byte_of_LEDs.c
@@ -3,39 +3,36 @@
  *
  *  Created on: Jan 1, 2025
  *      Author: Ln-Komandur
- * Creates tasks to light up LEDs connected via tha i2c bus with a PCF8574 expander.
+ * Creates a task to light up LEDs connected via the i2c bus with a PCF8574 IO expander.
  * LEDs light up on each pin of the PCF8574 based on bits written to them as a byte.
- * LEDs can also blink by shifting that byte left and reseting to the initial byte when all are off.
+ * LEDs also blink by shifting that byte left and reseting to the initial byte when all are off (0x00).
+ * The address of the PCF8574 IO expander is handled in the calling functions. 
+ * The functions below only get a device handle which is obtained by the calling function using a bus handle.
  */
 
 
-
 #include <a_byte_of_LEDs.h>
-#include <PCF8574.h>
+#include <PCF8574_IO_Expander.h>
 
-#define PCF8574_SLAVE_ADDR 0x21 // A0 jumper to ground
-
-#define TIMEOUT_MS		1000 // milliseconds
-
-
-static const char *TAG = "BYTE_ON_LEDs";
+static const char *TAG = "A_BYTE_ON_LEDs";
 
 uint8_t startByte; // This is the byte that is initialized
 
 int64_t lastBlinkTime; // When was the last blink
 
 /* Lights up a byte on the 8 LEDs connected to a PCF8574
- * Takes the byte to be lit up as the input
+ * Takes the device handle, and the byte to be light up as inputs
  */
 
-void show_byte_with_LEDs(uint8_t aByte)
+void show_byte_with_LEDs(i2c_master_dev_handle_t device_handle , uint8_t aByte)
 {
-	write_byte_to_pins(i2c_port_num, aByte);
+	write_byte_to_pins(device_handle ,aByte);
 	ESP_LOGI(TAG, "show_byte_with_LEDs");
 }
 
 /* Creates a task that blinks the 8 LEDs connected to a PCF8574. 
- * e.g. Starts with 0x55 as a byte and shifts it left after every `blinkDuration`. When the byte written is 0x00, it is reset to 0x55 again
+ * e.g. Starts with a byte (e.g. 0x55) and shifts it left after every `blinkDuration`. 
+ * When the byte written is 0x00, it is reset to the start byte (0x55) again.
  */
 
 
@@ -43,6 +40,7 @@ void blink_LEDs_Task(void *params)
 {
 	lastBlinkTime = esp_timer_get_time();
 	uint8_t aByte = startByte;
+	i2c_master_dev_handle_t device_handle = *(i2c_master_dev_handle_t *)params;
 
 	while (true)
 	{
@@ -61,12 +59,14 @@ void blink_LEDs_Task(void *params)
 		 */
 		if (callTime - lastBlinkTime > blinkDuration)
 		{
-			write_byte_to_pins(i2c_port_num, aByte);
+			write_byte_to_pins(device_handle, aByte);
+			//ESP_LOGI(TAG, "blinking LEDs by writing to pin");
+
 			aByte = aByte << 1;
 			if (aByte == 0x00) {
 				aByte = startByte;
 			}
-			lastBlinkTime = callTime;
+			lastBlinkTime = callTime; 
 		}
 	}
 	vTaskDelete(NULL); // added per https://stackoverflow.com/questions/63634917/freertos-task-should-not-return-esp32 at the end of the function to gracefully end the task:
@@ -74,12 +74,16 @@ void blink_LEDs_Task(void *params)
 
 /*
  * Initialize LED blinking task
+ * Creates a task with a 
+ *		* device handle
+ *		* blinkDuration
+ *		* startByte 
 */
-void init_LED_blink(i2c_port_t port_num, uint8_t initByte, long duration)
+void init_LED_blink(i2c_master_dev_handle_t device_handle, uint8_t initByte, long duration)
 {
-	i2c_port_num = port_num;
-	xTaskCreate(blink_LEDs_Task, "blink_LEDs_Task", 2048, NULL, 1, NULL);
 	blinkDuration = duration;
 	startByte = initByte;
+	xTaskCreate(blink_LEDs_Task, "blink_LEDs_Task", 2048, &device_handle, 1, NULL);
+	ESP_LOGI(TAG, "Created blink LEDs task");
 
 }
