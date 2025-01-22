72b7d0f99474c40b0b1ae95863884e69e8236cfb
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2025-01-22 08:58:21 -0600

    Initial

---
+++ NTP Timepiece/main/my_ntp_clock_main.c
@@ -0,0 +1,114 @@
+// Referenced from https://esp32tutorials.com/esp32-sntp-esp-idf-synchronize-time-ntp/ 
+/* LwIP SNTP example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_netif_sntp.h"
+#include <lwip/netdb.h>
+#include <wifi_connector.h>
+#include <i2c_LCD.h>
+
+
+#define NTP_SYNC_TIMEOUT_MS CONFIG_NTP_SYNC_TIMEOUT_MS
+#define NTP_SERVER CONFIG_SNTP_TIME_SERVER
+#define TIME_ZONE CONFIG_TIME_ZONE
+
+#define REFRESH_MS	1000 // In how many milliseconds should the clock time be refreshed
+
+ 
+static const char *TAG = "NTP Tutorial";
+
+time_t now;
+struct tm timeinfo;
+
+void Refresh_Every_Second_Task(void *params)
+{
+	
+	ESP_LOGI(TAG, "Now in Refresh_Every_Second_Task task");
+
+	UBaseType_t uxHighWaterMark; // Refer - https://www.freertos.org/Documentation/02-Kernel/04-API-references/03-Task-utilities/04-uxTaskGetStackHighWaterMark
+    /* Refer - https://www.freertos.org/Documentation/02-Kernel/04-API-references/03-Task-utilities/04-uxTaskGetStackHighWaterMark 
+    * Optionally inspect our own high water mark on entering the task.
+    */
+    // uxHighWaterMark = uxTaskGetStackHighWaterMark( NULL ); // Refer - https://www.freertos.org/Documentation/02-Kernel/04-API-references/03-Task-utilities/04-uxTaskGetStackHighWaterMark
+
+	char strftime_buf[64];
+	//char year[4];
+	char date[24];
+	
+	while (true)
+	
+	{
+		time(&now);
+		localtime_r(&now, &timeinfo);
+		strftime(strftime_buf, sizeof(strftime_buf), "%r", &timeinfo);
+		//strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+		//strftime(year, sizeof(year), "%Y", &timeinfo);
+		strftime(date, sizeof(date), "%a, %b-%d-%Y", &timeinfo);
+
+		//ESP_LOGI(TAG, "The current year is %s Year ", year);
+		ESP_LOGI(TAG, "The current date is %s ", date);
+		ESP_LOGI(TAG, "The current time in the %s Time Zone is: %s", TIME_ZONE, strftime_buf);
+		vTaskDelay( REFRESH_MS / portTICK_PERIOD_MS); // Delay for REFRESH_MS and count next. Since vTaskDelay takes only xTicksToDelay as argument, it has to be divided by portTICK_PERIOD_MS which is the number of milliseconds a scheduler TICK takes
+
+		/* 
+		Refer - https://www.freertos.org/Documentation/02-Kernel/04-API-references/03-Task-utilities/04-uxTaskGetStackHighWaterMark
+		Calling the function will have used some stack space, we would therefore now expect uxTaskGetStackHighWaterMark() to return a 
+       	value lower than when it was called on entering the task. 
+       	
+       	A task may query its own high water mark by passing NULL as the xTask parameter for the handle of the task being queried.
+       	*/
+        uxHighWaterMark = uxTaskGetStackHighWaterMark( NULL ); // Refer - https://www.freertos.org/FreeRTOS_Support_Forum_Archive/July_2019/freertos_Understanding_uxTaskGetStackHighWaterMark_results_51c44e8598j.html
+
+		ESP_LOGI(TAG, "Refresh_Every_Second_Task uxHighWaterMark = %u", uxHighWaterMark); // Refer - https://www.freertos.org/FreeRTOS_Support_Forum_Archive/July_2019/freertos_Understanding_uxTaskGetStackHighWaterMark_results_51c44e8598j.html
+	}
+	vTaskDelete(NULL); // added per https://stackoverflow.com/questions/63634917/freertos-task-should-not-return-esp32 at the end of the function to gracefully end the task:
+}
+
+
+void app_main(void)
+{
+	// Refer https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/system_time.html
+
+
+    time(&now);
+    localtime_r(&now, &timeinfo);
+
+	// Set timezone per the configuration file
+	setenv("TZ", TIME_ZONE, 1);
+	tzset();
+
+
+    // Is time set? If not, tm_year will be (1970 - 1900).
+    if (timeinfo.tm_year < (2016 - 1900)) {
+        ESP_LOGI(TAG, "Time is not set yet. Connecting to WiFi and getting time over NTP.");
+		init_nvs_flash();
+		connect_wifi();
+	
+		// Refer https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/system_time.html
+		esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG(NTP_SERVER);
+		esp_netif_sntp_init(&config);
+		if (esp_netif_sntp_sync_wait(pdMS_TO_TICKS(NTP_SYNC_TIMEOUT_MS)) != ESP_OK) 
+		{
+		    ESP_LOGI(TAG,"Failed to update system time within %ds timeout",NTP_SYNC_TIMEOUT_MS/1000);
+		}
+
+        // update 'now' variable with current time
+        time(&now);
+    }
+    else 
+    {
+        ESP_LOGI(TAG, "Time is ALREADY set in the RTC.");
+	}
+    
+	xTaskCreate(&Refresh_Every_Second_Task, "Refresh_Every_Second_Task", 2048, NULL, 1, NULL);
+	ESP_LOGI(TAG, "Created Refresh_Every_Second_Task");
+
+		
+}
